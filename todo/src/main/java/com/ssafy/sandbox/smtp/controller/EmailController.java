56583c8b06b065f02fd6dd5be00c9ebd7541ecdf
Yeop <syt05342@naver.com>
2024-11-01 01:20:22 +0900

    #2feat: SMTP 기능 구현

---
+++ todo/src/main/java/com/ssafy/sandbox/smtp/controller/EmailController.java
@@ -0,0 +1,30 @@
+package com.ssafy.sandbox.smtp.controller;
+
+import com.ssafy.sandbox.smtp.dto.EmailRequestDto;
+import com.ssafy.sandbox.smtp.dto.VerificationRequestDto;
+import com.ssafy.sandbox.smtp.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<?> sendEmail(@RequestBody EmailRequestDto requestDto) {
+        boolean isSent = emailService.sendVerificationCode(requestDto.getEmail());
+        return ResponseEntity.ok(Collections.singletonMap("isOk", isSent));
+    }
+
+    @PostMapping("/authentication")
+    public ResponseEntity<?> verifyCode(@RequestBody VerificationRequestDto requestDto) {
+        boolean isVerified = emailService.verifyCode(requestDto.getAuthentication());
+        return ResponseEntity.ok(Collections.singletonMap("isSuccess", isVerified));
+    }
+}
---
+++ todo/src/main/java/com/ssafy/sandbox/smtp/dto/VerificationRequestDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.sandbox.smtp.dto;
+
+import lombok.Data;
+
+@Data
+public class VerificationRequestDto {
+    private String authentication;
+}
